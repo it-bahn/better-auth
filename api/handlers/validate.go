33b4d0baf593e16f887e3139d5bd36dda9aed19f
daniyalibrahim <daniyal.ibrahim10@gmail.com>
2022-07-07 18:35:56 +0200

    try cors once more

--- api/handlers/validate.go
+++ api/handlers/validate.go
@@ -121,6 +121,7 @@ func EnableCors(w *http.ResponseWriter, req *http.Request) {
 	} else {
 		(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	}
+	(*w).Header().Set("Content-Type", "application/json")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
 }
