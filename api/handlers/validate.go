e6721f9fb044efef25980cbfe4d4284375797a6f
daniyalibrahim <daniyal.ibrahim10@gmail.com>
2022-07-08 19:47:16 +0200

    check

--- api/handlers/validate.go
+++ api/handlers/validate.go
@@ -116,13 +116,6 @@ func ValidateReqBody(w http.ResponseWriter, user users.User) map[string]interfac
 }
 
 func EnableCors(w *http.ResponseWriter, req *http.Request) {
-	(*w).Header().Set("Access-Control-Allow-Credentials", "true")
-	(*w).Header().Set("Access-Control-Allow-Origin", "*")
-	(*w).Header().Set("Allow-Control-Allow-Origin", "*")
-	(*w).Header().Set("Content-Type", "application/json")
-	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
-
 	(*w).Header().Add("Access-Control-Allow-Credentials", "true")
 	(*w).Header().Add("Access-Control-Allow-Origin", "*")
 	(*w).Header().Add("Allow-Control-Allow-Origin", "*")
